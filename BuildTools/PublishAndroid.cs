ac6e90c7218ea63d7b2c466ca0f748294c871606
Anstro Pleuton <anstro.pleuton@gmail.com>
2026-07-31 23:56:16 +0530

    Perhaps a useless publisher script

---
+++ BuildTools/PublishAndroid.cs
@@ -0,0 +1,43 @@
+// Useless looking script
+
+if (!OperatingSystem.IsWindows() && !OperatingSystem.IsLinux())
+{
+	Console.WriteLine("This script should be ran on Windows or Linux");
+	return;
+}
+
+if (Path.GetFileName(Directory.GetCurrentDirectory()) == "BuildTools")
+{
+	Directory.SetCurrentDirectory(Path.GetDirectoryName(Directory.GetCurrentDirectory())!);
+}
+
+if (Path.GetFileName(Directory.GetCurrentDirectory()) != "BPLE")
+{
+	Console.WriteLine("The script must be ran in BPLE directory");
+	return;
+}
+
+var psPath = Path.Combine(Directory.GetCurrentDirectory(), "ProjectSettings", "ProjectSettings.asset");
+
+var buildVersion = File.ReadLines(psPath)
+	.FirstOrDefault(line => line.Contains("bundleVersion"))!
+	.Split(':', 2)[1]
+	.Trim();
+
+var publishPath = Path.Combine(Directory.GetCurrentDirectory(), "Builds", "Publish");
+
+foreach (var currentBuild in (string[])["StandaloneLinux64"])
+{
+	var buildPath = Path.Combine(Directory.GetCurrentDirectory(), "Builds", currentBuild);
+
+	if (!Directory.Exists(buildPath))
+	{
+		Console.WriteLine($"Warning: Build for {currentBuild} does not exist: {buildPath}; Skipping it");
+		continue;
+	}
+
+	if (!Directory.Exists(publishPath)) Directory.CreateDirectory(publishPath);
+
+	var buildApk = Path.Combine(publishPath, $"BPLE-{buildVersion}-android.apk");
+	File.Copy(Path.Combine(buildPath, $"BPLE-{buildVersion}.apk"), buildApk);
+}
